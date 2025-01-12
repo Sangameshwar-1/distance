922b83d9875e96906bc196b1e1df696b37d44a6b
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-01-12 23:27:41 +0530

    Delete random_number_project directory

--- random_number_project/urls.py
+++
@@ -1,8 +0,0 @@
-# random_number_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('random_number.urls')),  # Include the random_number app's URLs
-]
--- random_number_project/wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_number_project.settings')
-
-application = get_wsgi_application()
